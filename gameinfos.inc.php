7f38009e1589716cd2c1f31717cb04bcc6d70a32
Guy Baudouin <guythoun@gmail.com>
2024-03-27 17:51:23 +0100

    fix gamedatas

--- gameinfos.inc.php
+++ gameinfos.inc.php
@@ -13,16 +13,7 @@
 $gameinfos = [
 
 // Name of the game in English (will serve as the basis for translation) 
-'game_name' => "GlowExpansion",
-
-// Game designer (or game designers, separated by commas)
-'designer' => 'Cédrick Chaboussit',       
-
-// Game artist (or game artists, separated by commas)
-'artist' => 'Ben Basso, Vincent Dutrait',         
-
-// Year of FIRST publication of this game. Can be negative.
-'year' => 2021,
+'game_name' => "Glow",
 
 // Game publisher (use empty string if there is no publisher)
 'publisher' => 'Bombyx',
@@ -87,18 +78,6 @@ $gameinfos = [
 // NB: the default will be the first language in this list spoken by the player, so you should list them by popularity/preference.
 'language_dependency' => false,
 
-// Complexity of the game, from 0 (extremely simple) to 5 (extremely complex)
-'complexity' => 2,    
-
-// Luck of the game, from 0 (absolutely no luck in this game) to 5 (totally luck driven)
-'luck' => 3,    
-
-// Strategy of the game, from 0 (no strategy can be setup) to 5 (totally based on strategy)
-'strategy' => 3,    
-
-// Diplomacy of the game, from 0 (no interaction in this game) to 5 (totally based on interaction and discussion between players)
-'diplomacy' => 3,    
-
 // Colors attributed to players
 'player_colors' => ['ff0000', '008000', '0000ff', 'ffa500', 'e94190', '982fff', '72c3b1', 'f07f16', 'bdd002', '7b7b7b'],
 
@@ -126,29 +105,6 @@ $gameinfos = [
     //  minimum possible value: 740
     'max' => null
 ],
-
-// Game presentation
-// Short game presentation text that will appear on the game description page, structured as an array of paragraphs.
-// Each paragraph must be wrapped with totranslate() for translation and should not contain html (plain text without formatting).
-// A good length for this text is between 100 and 150 words (about 6 to 9 lines on a standard display)
-'presentation' => [ // taken from bombys website
-    totranslate("The green of the trees and the outlines of the volcanoes can no longer be distinguished. Complete silence. Dispelling this darkness and restoring color to the world requires many bursts of light."), 
-    totranslate("Adventurers are born and trained for this: to master the power of the elements, choose their companions and find their way to the villages with light.")
-],
-
-'custom_buy_button' => array(
-    'url' => 'https://www.philibertnet.com/en/bombyx/93241-lueur-3760267990526.html',
-    'label' => 'Philibert',
- ),
-
-// Games categories
-//  You can attribute a maximum of FIVE "tags" for your game.
-//  Each tag has a specific ID (ex: 22 for the category "Prototype", 101 for the tag "Science-fiction theme game")
-//  Please see the "Game meta information" entry in the BGA Studio documentation for a full list of available tags:
-//  http://en.doc.boardgamearena.com/Game_meta-information:_gameinfos.inc.php
-//  IMPORTANT: this list should be ORDERED, with the most important tag first.
-//  IMPORTANT: it is mandatory that the FIRST tag is 1, 2, 3 and 4 (= game category)
-'tags' => [2, 12, 100, 103, 104, 201],
 
 
 //////// BGA SANDBOX ONLY PARAMETERS (DO NOT MODIFY)
